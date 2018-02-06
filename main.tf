e4ffafd9840d1e9fc1fcfb0cd71d559e791da2d9
mt10ib <mt10ib@gamil.com>
2018-02-05 19:37:16 -0500

    first commit

--- main.tf
+++ main.tf
@@ -7,7 +7,6 @@ provider "aws" {
 # Create an instance with ami-2d39803a
 resource "aws_instance" "example" {
   
-  count         = 1
   ami           = "ami-2d39803a"
   instance_type = "t2.micro"
 #  security_groups = ["${aws_security_group.allow_ssh.id}"]
